88a5685fec1562387c4a09f8b2a7304dd865e9d5
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-06 23:24:20 -0400

    First route implemented

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+// get route
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to the bookstore');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to the port: ${PORT}`);
 });
