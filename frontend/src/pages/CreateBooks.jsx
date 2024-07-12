c24215754b1982e39ce09e87be353ed469d5ba50
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-11 23:33:22 -0400

    State and functionality added to create books file

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBooks = () => {
+  // states for form data 
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // function to handle book creation
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occurred. Please try again.');
+        console.log(error);
+      })
+  };
+
   return (
     <div>CreateBooks</div>
   )
