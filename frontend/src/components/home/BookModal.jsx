6597a7bd73308e5c10ed61be05579a1bfde965c6
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-12 22:45:21 -0400

    Styling updated for cards, and for modal

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -6,17 +6,18 @@ import { BiUserCircle } from 'react-icons/bi';
 const BookModal = ({ book, onClose }) => {
   return (
     <div className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center' onClick={onClose}>
-        <div onClick={(event) => event.stopPropagation()} className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'>
-            <AiOutlineClose className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer' onClick={onClose}/>
-            <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
-            <h4 className='my-2 text-gray-500'>{book._id}</h4>
-            <div className='flex justify-start items-center gap-x-2'>
-                <PiBookOpenTextLight className='text-red-300 text-2xl' />
-                <h2 className='my-1'>{book.title}</h2>
+        {/* The modal itself */}
+        <div onClick={(event) => event.stopPropagation()} className='w-[600px] max-w-full h-[400px] border rounded-xl p-4 flex flex-col relative bg-zinc-950'>
+            <AiOutlineClose className='absolute right-6 top-6 text-3xl text-red-600 hover:text-white hover: cursor-pointer bg-zinc-950' onClick={onClose}/>
+            <h2 className='w-fit px-4 py-1 bg-sky-300 text-black rounded-lg'>{book.publishYear}</h2>
+            <h4 className='my-2 text-gray-500 bg-zinc-950'>{book._id}</h4>
+            <div className='flex justify-start items-center gap-x-2 bg-zinc-950'>
+                <PiBookOpenTextLight className='text-sky-300 text-2xl bg-zinc-950' />
+                <h2 className='my-1 bg-zinc-950'>{book.title}</h2>
             </div>
-            <div className='flex justify-start items-center gap-x-2'>
-                <BiUserCircle className='text-red-300 text-2xl' />
-                <h2 className='my-1'>{book.author}</h2>
+            <div className='flex justify-start items-center gap-x-2 bg-zinc-950'>
+                <BiUserCircle className='text-sky-300 text-2xl bg-zinc-950' />
+                <h2 className='my-1 bg-zinc-950'>{book.author}</h2>
             </div>
         </div>
     </div>
--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -12,27 +12,27 @@ const BookSingleCard = ({book}) => {
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
-            <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
+        <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl text-white'>
+            <h2 className='absolute top-1 right-2 px-4 py-1 bg-sky-300 rounded-lg text-black'>{book.publishYear}</h2>
             <h4 className='my-2 text-gray-500'>{book._id}</h4>
             <div className='flex justify-start items-center gap-x-2'>
-                <PiBookOpenTextLight className='text-red-300 text-2xl' />
+                <PiBookOpenTextLight className='text-sky-300 text-2xl' />
                 <h2 className='my-1'>{book.title}</h2>
             </div>
             <div className='flex justify-start items-center gap-x-2'>
-                <BiUserCircle className='text-red-300 text-2xl' />
+                <BiUserCircle className='text-sky-300 text-2xl' />
                 <h2 className='my-1'>{book.author}</h2>
             </div>
             <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
-                <BiShow className='text-3xl text-blue-800 hover:text-black cursor-pointer' onClick={() => setShowModal(true)} />
+                <BiShow className='text-3xl hover:text-blue-600 cursor-pointer' onClick={() => setShowModal(true)} />
                 <Link to={`/books/details/${book._id}`}>
-                    <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
+                    <BsInfoCircle className='text-2xl hover:text-green-600' />
                 </Link>
                 <Link to={`/books/edit/${book._id}`}>
-                    <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
+                    <AiOutlineEdit className='text-2xl hover:text-yellow-600' />
                 </Link>
                 <Link to={`/books/delete/${book._id}`}>
-                    <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
+                    <MdOutlineDelete className='text-2xl hover:text-red-600' />
                 </Link>
             </div>
         {
