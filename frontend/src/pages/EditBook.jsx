05d6d9a8c5175b2bb87aa138c64b2c931af476fc
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-12 22:51:53 -0400

    Remaining styling changes implemented

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -66,15 +66,15 @@ const EditBook = () => {
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full' />
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full bg-white' />
         </div>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Author</label>
-          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full' />
+          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full bg-white' />
         </div>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
-          <input type="text" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full' />
+          <input type="text" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full bg-white' />
         </div>
         <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>Save</button>
       </div>
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -32,43 +32,45 @@ const ShowBook = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-          <div className='my-4'>
-            {/* Book ID */}
-            <span className='text-xl mr-4 text-gray-500'>ID</span>
-            <span>{book._id}</span>
-          </div>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4 text-white'>
+            <div className='my-4'>
+              {/* Book ID */}
+              <span className='text-xl mr-4 text-gray-500'>ID</span>
+              <span>{book._id}</span>
+            </div>
 
-          <div className='my-4'>
-            {/* Book title */}
-            <span className='text-xl mr-4 text-gray-500'>Title</span>
-            <span>{book.title}</span>
-          </div>
+            <div className='my-4'>
+              {/* Book title */}
+              <span className='text-xl mr-4 text-gray-500'>Title</span>
+              <span>{book.title}</span>
+            </div>
 
-          <div className='my-4'>
-            {/* Book author */}
-            <span className='text-xl mr-4 text-gray-500'>Author</span>
-            <span>{book.author}</span>
-          </div>
+            <div className='my-4'>
+              {/* Book author */}
+              <span className='text-xl mr-4 text-gray-500'>Author</span>
+              <span>{book.author}</span>
+            </div>
 
-          <div className='my-4'>
-            {/* Book publishYear */}
-            <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
-            <span>{book.publishYear}</span>
-          </div>
+            <div className='my-4'>
+              {/* Book publishYear */}
+              <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+              <span>{book.publishYear}</span>
+            </div>
 
-          <div className='my-4'>
-            {/* Book created */}
-            <span className='text-xl mr-4 text-gray-500'>Addition Time</span>
-            <span>{new Date(book.createdAt).toString()}</span>
-          </div>
+            <div className='my-4'>
+              {/* Book created */}
+              <span className='text-xl mr-4 text-gray-500'>Addition Time</span>
+              <span>{new Date(book.createdAt).toString()}</span>
+            </div>
 
-          <div>
-            {/* Book updated */}
-            <span className='text-xl mr-4 text-gray-500'>Updated Time</span>
-            <span>{new Date(book.updatedAt).toString()}</span>
-          </div>
+            <div>
+              {/* Book updated */}
+              <span className='text-xl mr-4 text-gray-500'>Updated Time</span>
+              <span>{new Date(book.updatedAt).toString()}</span>
+            </div>
 
+          </div>
         </div>
       )}
     </div>
